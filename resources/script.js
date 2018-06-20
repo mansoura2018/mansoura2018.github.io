df690c792b3b5e2b93f35b99bcaee78a6898d095
Abdelhady <abdelhady.saad@hotmail.com>
2018-06-20 04:31:43 +0200

    website upload

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,7 @@
 var ah3 = document.querySelector("h3");
 var btn1 = document.querySelector(".btn-full");
+var btn2 = document.querySelector(".btn-ghost");
+var longCopy = document.querySelector(".longCopy");
 
 btn1.addEventListener("click", function(){
     ah3.classList.toggle("dis");
@@ -7,4 +9,8 @@ btn1.addEventListener("click", function(){
 
 ah3.addEventListener("mouseover", function(){
     ah3.className = "redtxt dis";
+})
+
+btn2.addEventListener("click", function(){
+    longCopy.classList.toggle("dis");
 })
